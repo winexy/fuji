c2d25919aa11989927dc868c337bac4cfc9e285b
Olzhas Seitmagambet <sei7magambet@gmail.com>
2020-10-21 23:48:17 +0600

    refactor(rules): refactor string tests

--- src/rules/string.spec.ts
+++ src/rules/string.spec.ts
@@ -4,35 +4,31 @@ import { createContext } from '../utils';
 describe('rules.string', () => {
   let rule = null;
   beforeEach(() => {
-    rule = string('error');
+    rule = string('string error');
   });
-  
-  it('should push error for number', () => {
-    const res = rule(createContext(42));
 
-    expect(res.errors).toBeArrayOfSize(1);
-    expect(res.errors[0]).toHaveProperty('message', 'error');
-    expect(res.errors[0]).toHaveProperty('type', 'string');
+  it('should push provided message for invalid value', () => {
+    const { errors } = rule(createContext({}));
+    expect(errors[0]).toHaveProperty('message', 'string error');
+  });
+
+  it('should push error for number', () => {
+    const { errors } = rule(createContext(42));
+    expect(errors).toBeArrayOfSize(1);
   });
 
   it('should push error for boolean', () => {
-    const res = rule(createContext(false));
-
-    expect(res.errors).toBeArrayOfSize(1);
-    expect(res.errors[0]).toHaveProperty('message', 'error');
-    expect(res.errors[0]).toHaveProperty('type', 'string');
+    const { errors } = rule(createContext(false));
+    expect(errors).toBeArrayOfSize(1);
   });
 
   it('should push error for array', () => {
-    const res = rule(createContext([]));
-
-    expect(res.errors).toBeArrayOfSize(1);
-    expect(res.errors[0]).toHaveProperty('message', 'error');
-    expect(res.errors[0]).toHaveProperty('type', 'string');
+    const { errors } = rule(createContext([]));
+    expect(errors).toBeArrayOfSize(1);
   });
 
   it('should not push error for string type', () => {
     const res = rule(createContext('hello'));
-    expect(res.errors).toBeArrayOfSize(0);
+    expect(res.errors).toBeEmpty();
   });
 });
