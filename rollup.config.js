293a7c36be2f9b2849e5e71bb501e5a7633e1346
Olzhas Seitmagambet <sei7magambet@gmail.com>
2020-10-22 21:46:18 +0600

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,15 @@ import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'es'
-  },
+  output: [
+    {
+      file: 'dist/index.es.js',
+      format: 'es'
+    },
+    {
+      file: 'dist/index.js',
+      format: 'cjs'
+    }
+  ],
   plugins: [ts({ lib: ['es6'] }), terser()]
 };
