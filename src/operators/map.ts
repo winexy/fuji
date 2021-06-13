22fbba7b9840e0e1dc3f4aaf2e410b08f959b94e
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-14 01:44:42 +0300

    refactor: remove "result" key from map result

--- src/operators/map.ts
+++ src/operators/map.ts
@@ -2,12 +2,9 @@ import { VFunc } from '../types'
 
 export const map = <A, B>(f: (a: A) => B): VFunc<A, B> => {
   return function MapFunc(ctx) {
-    const next = f(ctx.current)
-    
     return {
       ...ctx,
-      current: next,
-      result: next
+      current: f(ctx.current)
     }
   }
 }
