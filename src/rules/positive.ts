c1a0f11387b25589129553d7097e7a31f274a146
Olzhas Seitmagambet <sei7magambet@gmail.com>
2021-06-13 16:43:42 +0300

    fix: add number check for positive rule

--- src/rules/positive.ts
+++ src/rules/positive.ts
@@ -1,11 +1,11 @@
 import type { VFunc } from '../types'
-import { createError } from '../utils'
+import { createError, isNumber } from '../utils'
 
 export type PositiveType = 'positive'
 
 export const positive = (msg?: string): VFunc<number> => {
   return function PositiveV8N(ctx) {
-    if (ctx.current < 0) {
+    if (!isNumber(ctx.current) || ctx.current < 0) {
       ctx.errors.push(createError('positive', msg, ctx))
     }
 
